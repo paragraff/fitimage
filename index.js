ce4617eac7219fc9527458f0bb4c96e36f9ccc0f
Oleksandr Kovtiukh <a.kovtukh@gmail.com>
2019-11-14 19:24:06 +0200

    easy ajax upload files

--- index.js
+++ index.js
@@ -1,30 +1,12 @@
-const path = require('path')
-const fs = require('fs')
 const express = require('express')
-const bodyParser= require('body-parser')
 const app = express()
 const multer = require('multer')
 
-const parseFile = function(file, req) {
-  var parsedFile = path.parse(file),
-    fullUrl = req.protocol + '://' + req.get('host') + '/uploads/';
-
-  return {
-    name: parsedFile.name,
-    base: parsedFile.base,
-    extension: parsedFile.ext.substring(1),
-    url: fullUrl + parsedFile.base,
-    size: bytes(fs.statSync(file).size)
-  };
-};
-
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
 app.use(express.static(__dirname + '/public'))
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'public/uploads/')
+    cb(null, 'public/images/')
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname)
